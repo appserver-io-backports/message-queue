4fe4374477bc7b52a3171ef8ab6badbb5ef3c240
zelgerj <zelger@me.com>
2013-08-08 21:48:39 +0200

    refactored application class

--- src/TechDivision/MessageQueue/Application.php
+++ src/TechDivision/MessageQueue/Application.php
@@ -12,6 +12,7 @@
     
 namespace TechDivision\MessageQueue;
 
+use TechDivision\ApplicationServer\AbstractApplication;
 use TechDivision\MessageQueue\Service\Locator\QueueLocator;
 
 /**
@@ -24,48 +25,14 @@ use TechDivision\MessageQueue\Service\Locator\QueueLocator;
  *              Open Software License (OSL 3.0)
  * @author      Tim Wagner <tw@techdivision.com>
  */
-class Application {
-
-    /**
-     * Path to the container's host configuration.
-     * @var string
-     */
-    const CONTAINER_HOST = '/container/host';
-
-    /**
-     * Path to the container's base directory.
-     * @var string
-     */
-    const CONTAINER_BASE_DIRECTORY = '/container/baseDirectory';
-    
-    /**
-     * The unique application name.
-     * @var string
-     */
-    protected $name;
-
+class Application extends AbstractApplication
+{
     /**
      * The queue manager.
      * @var \TechDivision\MessageQueue\QueueManager
      */
     protected $queueManager;
 
-    /**
-     * The host configuration.
-     * @var \TechDivision\ApplicationServer\Configuration
-     */
-    protected $configuration;
-    
-    /**
-     * Passes the application name That has to be the class namespace.
-     * 
-     * @param type $name The application name
-     */
-    public function __construct($initialContext, $name) {
-        $this->initialContext = $initialContext;
-        $this->name = $name;
-    }
-    
     /**
      * Has been automatically invoked by the container after the application
      * instance has been created.
@@ -85,57 +52,7 @@ class Application {
         // return the instance itself
         return $this;
     }
-    
-    /**
-     * Returns the application name (that has to be the class namespace, 
-     * e. g. TechDivision\Example).
-     * 
-     * @return string The application name
-     */
-    public function getName() {
-        return $this->name;
-    }
 
-    /**
-     * Set's the host configuration.
-     *
-     * @param \TechDivision\ApplicationServer\Configuration $configuration The host configuration
-     * @return \TechDivision\ServletContainer\Application The application instance
-     */
-    public function setConfiguration($configuration) {
-        $this->configuration = $configuration;
-        return $this;
-    }
-
-    /**
-     * Returns the host configuration.
-     *
-     * @return \TechDivision\ApplicationServer\Configuration The host configuration
-     */
-    public function getConfiguration() {
-        return $this->configuration;
-    }
-
-    /**
-     * Returns the path to the appserver webapp base directory.
-     *
-     * @return string The path to the appserver webapp base directory
-     */
-    public function getAppBase() {
-        $baseDir = $this->getConfiguration()->getChild(self::CONTAINER_BASE_DIRECTORY)->getValue();
-        $appBase = $this->getConfiguration()->getChild(self::CONTAINER_HOST)->getAppBase();
-        return $baseDir . $appBase;
-    }
-
-    /**
-     * Return's the path to the web application.
-     *
-     * @return string The path to the web application
-     */
-    public function getWebappPath() {
-        return $this->getAppBase() . DS . $this->getName();
-    }
-    
     /**
      * Sets the applications queue manager instance.
      * 
