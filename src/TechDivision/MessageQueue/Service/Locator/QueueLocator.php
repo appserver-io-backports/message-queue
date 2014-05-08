f4664dcdebc8d6c71cdb095eb9d1b3d3455eb510
Tim Wagner <t.wagner@techdivision.com>
2014-05-08 18:22:17 +0200

    Solving merge conflicts

--- src/TechDivision/MessageQueue/Service/Locator/QueueLocator.php
+++ src/TechDivision/MessageQueue/Service/Locator/QueueLocator.php
@@ -1,10 +1,10 @@
 <?php
+
 /**
  * TechDivision\MessageQueue\Service\Locator\QueueLocator
  *
- * PHP version 5
+ * NOTICE OF LICENSE
  *
-<<<<<<< HEAD
  * This source file is subject to the Open Software License (OSL 3.0)
  * that is available through the world-wide-web at this URL:
  * http://opensource.org/licenses/osl-3.0.php
@@ -19,14 +19,6 @@
  * @copyright  2014 TechDivision GmbH <info@techdivision.com>
  * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * @link       https://github.com/techdivision/TechDivision_MessageQueue
-=======
- * @category   Appserver
- * @package    TechDivision_MessageQueue
- * @subpackage Service
- * @author     Tim Wagner <tw@techdivision.com>
- * @copyright  2013 TechDivision GmbH <info@techdivision.com>
- * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
->>>>>>> 0a313ccad73381fa07933b6fb5a3d8b3d6c76d5b
  * @link       http://www.appserver.io
  */
 
@@ -40,7 +32,6 @@ use TechDivision\MessageQueue\Service\Locator\ResourceLocatorInterface;
 /**
  * The queue resource locator implementation.
  *
-<<<<<<< HEAD
  * @category   Library
  * @package    TechDivision_MessageQueue
  * @subpackage Service
@@ -49,14 +40,6 @@ use TechDivision\MessageQueue\Service\Locator\ResourceLocatorInterface;
  * @copyright  2014 TechDivision GmbH <info@techdivision.com>
  * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * @link       https://github.com/techdivision/TechDivision_MessageQueue
-=======
- * @category   Appserver
- * @package    TechDivision_MessageQueue
- * @subpackage Service
- * @author     Tim Wagner <tw@techdivision.com>
- * @copyright  2013 TechDivision GmbH <info@techdivision.com>
- * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
->>>>>>> 0a313ccad73381fa07933b6fb5a3d8b3d6c76d5b
  * @link       http://www.appserver.io
  */
 class QueueLocator implements ResourceLocatorInterface
@@ -73,12 +56,9 @@ class QueueLocator implements ResourceLocatorInterface
      * Initializes the locator with the actual queue manager instance.
      *
      * @param \TechDivision\MessageQueue\QueueManager $queueManager The queue manager instance
+     * @return void
      */
-<<<<<<< HEAD
     public function __construct(QueueManager $queueManager)
-=======
-    public function __construct($queueManager)
->>>>>>> 0a313ccad73381fa07933b6fb5a3d8b3d6c76d5b
     {
         $this->queueManager = $queueManager;
     }
@@ -86,50 +66,20 @@ class QueueLocator implements ResourceLocatorInterface
     /**
      * Tries to locate the servlet that handles the request and returns the instance if one can be found.
      *
-<<<<<<< HEAD
      * @param \TechDivision\MessageQueueProtocol\Queue $queue
      *
      * @return \TechDivision\MessageQueueProtocol\Queue
-=======
-     * @param \TechDivision\MessageQueueClient\Queue $queue The queue client instance
-     *
-     * @return \TechDivision\MessageQueueClient\Interfaces\MessageReceiver
->>>>>>> 0a313ccad73381fa07933b6fb5a3d8b3d6c76d5b
      * @see \TechDivision\MessageQueue\Service\Locator\ResourceLocatorInterface::locate()
      */
     public function locate(Queue $queue)
     {
-<<<<<<< HEAD
 
         // load registered queues and requested queue name
         $queues = $this->queueManager->getQueues();
 
-=======
-        // load registered queues and requested queue name
-        $queues = $this->queueManager->getQueues();
-        $queueName = $queue->getName();
-   
->>>>>>> 0a313ccad73381fa07933b6fb5a3d8b3d6c76d5b
         // return Receiver of requested queue if available
         if (array_key_exists($queueName = $queue->getName(), $queues)) {
             return $queues[$queueName];
         }
     }
-<<<<<<< HEAD
-=======
-    
-    /**
-     * Creates a new instance of the passed class name and passes the
-     * args to the instance constructor.
-     * 
-     * @param string $className The class name to create the instance of
-     * @param array  $args      The parameters to pass to the constructor
-     *
-     * @return object The created instance
-     */
-    public function newInstance($className, array $args = array())
-    {
-        return $this->queueManager->newInstance($className, $args);
-    }
->>>>>>> 0a313ccad73381fa07933b6fb5a3d8b3d6c76d5b
 }
--- src/TechDivision/MessageQueue/Service/Locator/ResourceLocatorInterface.php
+++ src/TechDivision/MessageQueue/Service/Locator/ResourceLocatorInterface.php
@@ -1,10 +1,10 @@
 <?php
+
 /**
  * TechDivision\MessageQueue\Service\Locator\ResourceLocatorInterface
  *
- * PHP version 5
+ * NOTICE OF LICENSE
  *
-<<<<<<< HEAD
  * This source file is subject to the Open Software License (OSL 3.0)
  * that is available through the world-wide-web at this URL:
  * http://opensource.org/licenses/osl-3.0.php
@@ -12,21 +12,13 @@
  * PHP version 5
  *
  * @category   Library
-=======
- * @category   Appserver
->>>>>>> 0a313ccad73381fa07933b6fb5a3d8b3d6c76d5b
  * @package    TechDivision_MessageQueue
  * @subpackage Service
  * @author     Tim Wagner <tw@techdivision.com>
  * @author     Markus Stockbauer <ms@techdivision.com>
-<<<<<<< HEAD
  * @copyright  2014 TechDivision GmbH <info@techdivision.com>
  * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * @link       https://github.com/techdivision/TechDivision_MessageQueue
-=======
- * @copyright  2013 TechDivision GmbH <info@techdivision.com>
- * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
->>>>>>> 0a313ccad73381fa07933b6fb5a3d8b3d6c76d5b
  * @link       http://www.appserver.io
  */
 
@@ -37,41 +29,25 @@ use TechDivision\MessageQueueProtocol\Queue;
 /**
  * Interface for the resource locator instances.
  *
-<<<<<<< HEAD
  * @category   Library
-=======
- * @category   Appserver
->>>>>>> 0a313ccad73381fa07933b6fb5a3d8b3d6c76d5b
  * @package    TechDivision_MessageQueue
  * @subpackage Service
  * @author     Tim Wagner <tw@techdivision.com>
  * @author     Markus Stockbauer <ms@techdivision.com>
-<<<<<<< HEAD
  * @copyright  2014 TechDivision GmbH <info@techdivision.com>
  * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * @link       https://github.com/techdivision/TechDivision_MessageQueue
-=======
- * @copyright  2013 TechDivision GmbH <info@techdivision.com>
- * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
->>>>>>> 0a313ccad73381fa07933b6fb5a3d8b3d6c76d5b
  * @link       http://www.appserver.io
  */
-interface ResourceLocatorInterface
-{
+interface ResourceLocatorInterface {
 
     /**
      * Tries to locate the servlet that handles the request and returns the instance if one can be found.
      *
      * @param \TechDivision\MessageQueueProtocol\Queue $queue
      *
-<<<<<<< HEAD
      * @return \TechDivision\MessageQueueProtocol\Queue
      * @see \TechDivision\MessageQueue\Service\Locator\ResourceLocatorInterface::locate()
-=======
-     * @param \TechDivision\MessageQueueClient\Queue $queue The queue client instance
-     *
-     * @return \TechDivision\MessageQueueClient\Interfaces\MessageReceiver The receiver that handles the passed queue
->>>>>>> 0a313ccad73381fa07933b6fb5a3d8b3d6c76d5b
      */
     public function locate(Queue $queue);
 }
