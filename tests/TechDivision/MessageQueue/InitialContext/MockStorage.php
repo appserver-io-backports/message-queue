aa0b9e3a1f40bbcfc9b81092aeca52ac1ea647fa
wick-ed <b.wick@techdivision.com>
2014-07-22 10:15:29 +0200

    cleaned up old tests

--- tests/TechDivision/MessageQueue/InitialContext/MockStorage.php
+++ tests/TechDivision/MessageQueue/InitialContext/MockStorage.php
@@ -1,13 +1,13 @@
 <?php
-
 /**
- * TechDivision\MessageQueue\InitialContext\MockStorage
+ * PHP version 5
  *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
+ * @category  Appserver
+ * @package   TechDivision_MessageQueue
+ * @author    Tim Wagner <tw@techdivision.com>
+ * @copyright 2013 TechDivision GmbH <info@techdivision.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      http://www.appserver.io
  */
 
 namespace TechDivision\MessageQueue\InitialContext;
